22c2fe7462a9d08f46232cc33f95564bc5e51f95
Malte Londschien <malte@londschien.de>
2024-06-08 16:20:40 +0200

    Add args to guggenberger12_size.

--- experiments/guggenberger12_size_collect.py
+++ experiments/guggenberger12_size_collect.py
@@ -8,6 +8,7 @@ os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 # isort: on
 
+import click
 import numpy as np
 import pandas as pd
 from ivmodels.simulate import simulate_guggenberger12
@@ -36,18 +37,20 @@ tests = {
 mx = 1
 mw = 1
 m = mx + mw
-n = 1000
 
 
-def _run(seed, k):
-    Z, X, y, _, W, beta = simulate_guggenberger12(n, k=k, seed=seed, return_beta=True)
+def _run(seed, n, k):
+    Z, X, y, _, W, beta = simulate_guggenberger12(n=n, k=k, seed=seed, return_beta=True)
     return {
         test_name: test(Z=Z, X=X, y=y, W=W, beta=beta, fit_intercept=False)[1]
         for test_name, test in tests.items()
     }
 
 
-if __name__ == "__main__":
+@click.command()
+@click.option("--n", default=1000)
+@click.option("--n_cores", default=-1)
+def main(n, n_cores):
     import itertools
 
     from ivmodels_simulations.constants import DATA_PATH
@@ -59,8 +62,11 @@ if __name__ == "__main__":
     n_seeds = 10000
     ks = [5, 10, 15, 20, 25, 30]
 
-    pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)
-    result = pool.starmap(_run, itertools.product(range(n_seeds), ks))
+    if n_cores == -1:
+        n_cores = multiprocessing.cpu_count() - 1
+
+    pool = multiprocessing.Pool(n_cores)
+    result = pool.starmap(partial(_run, n=n), itertools.product(range(n_seeds), ks))
 
     p_values = {(test_name, k): np.zeros(n_seeds) for test_name in tests for k in ks}
     for idx, (seed, k) in enumerate(itertools.product(range(n_seeds), ks)):
@@ -69,5 +75,9 @@ if __name__ == "__main__":
 
     columns = pd.MultiIndex.from_tuples(p_values.keys())
     pd.DataFrame(p_values, columns=columns).to_csv(
-        output / "guggenberger12_p_values.csv", index=False
+        output / f"guggenberger12_p_values_n={n}.csv", index=False
     )
+
+
+if __name__ == "__main__":
+    main()
