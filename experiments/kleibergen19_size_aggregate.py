91441e479098e6d038df04503855554a2a273c7a
Malte Londschien <malte@londschien.de>
2024-06-10 17:18:48 +0200

    Colormap.

--- experiments/kleibergen19_size_aggregate.py
+++ experiments/kleibergen19_size_aggregate.py
@@ -1,12 +1,13 @@
-import numpy as np
 import click
+import cmap
 import h5py
-from kleibergen19_size_collect import tests
 import matplotlib.pyplot as plt
-from matplotlib import cm
+import numpy as np
+
 from ivmodels_simulations.constants import DATA_PATH
 
 output = DATA_PATH / "kleibergen19_size"
+
 
 @click.command()
 @click.option("--n", default=1000)
@@ -26,43 +27,68 @@ def main(n, k, n_vars, lambda_max, n_seeds, cov_type):
     p_values = {}
 
     tests = [
-        "LM (LIML)",
-        # "Wald (TSLS)",
-        "Wald (LIML)",
-        "LR",
-        "LM",
         "AR",
+        "AR (Guggenberger)",
         "CLR",
-        # "AR (Guggenberger)",
+        "LM",
+        "LM (LIML)",
+        "LR",
+        "Wald (LIML)",
+        "Wald (TSLS)",
     ]
-
 
     for test_name in tests:
         p_values[test_name] = file[test_name]["p_values"][()]
 
+    fig_width = 1.5 * 6.3
+    fig_height = 1.5 * 4.725 * 5 / 3
+    fig, axes = plt.subplots(
+        nrows=4,
+        ncols=2,
+        subplot_kw={"projection": "3d"},
+        figsize=(fig_width, fig_height),
+    )
 
-    max_rejection = np.max([np.max((v < 0.05).mean(axis=0).max(axis=0)) for v in p_values.values()])
-
-    fig, axes = plt.subplots(nrows=3, ncols=2, subplot_kw={"projection": "3d"})
-
+    my_cmap = cmap.Colormap(
+        [(0.0, "blue"), (0.05, "green"), (0.1, "yellow"), (0.5, "red"), (1, "red")]
+    ).to_mpl()
+    # cmap = "PuBu_r"# "viridis"
+    norm = None  # PowerNorm(gamma=0.5) # , linear_width=0.05)
     surfaces = []
 
     for idx, (ax, test_name) in enumerate(zip(axes.flat, tests)):
         # Plot the surface.
-        surfaces.append(ax.plot_surface(lambda_1s, lambda_2s, (p_values[test_name] < 0.05).mean(axis=0).max(axis=0), cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False, vmin=0, vmax=max_rejection))
+        surfaces.append(
+            ax.plot_surface(
+                lambda_1s,
+                lambda_2s,
+                (p_values[test_name] < 0.05).mean(axis=0).max(axis=0),
+                cmap=my_cmap,
+                norm=norm,
+                linewidth=0,
+                antialiased=False,
+                vmin=0,
+                vmax=1,
+            )
+        )
+        # ax.contour3D(lambda_1s, lambda_2s, (p_values[test_name] < 0.05).mean(axis=0).max(axis=0), levels=[0.05, 0.1, 0.15, 0.2])
         ax.set_title(test_name)
 
         # Customize the z axis.
-        ax.set_zlim(0, max_rejection * 1.1)
+        # ax.set_zlim(0, max_rejection * 1.1)
 
         # rotate the axes such that 0, 0, 0 is in the front
         ax.view_init(elev=20, azim=45)
 
-    fig.colorbar(surfaces[0], ax=axes.ravel().tolist(), shrink=0.5, aspect=5)
+    fig.colorbar(surfaces[-1], ax=axes.ravel().tolist(), shrink=0.5, aspect=5)
 
-    fig.tight_layout()
-    plt.savefig(output / f"kleibergen19_size_n={n}_k={k}_n_seeds={n_seeds}_n_vars={n_vars}_lambda_max={lambda_max}_cov_type={cov_type}.png")
+    # fig.tight_layout()
+    plt.show()
+    plt.savefig(
+        output
+        / f"kleibergen19_size_n={n}_k={k}_n_seeds={n_seeds}_n_vars={n_vars}_lambda_max={lambda_max}_cov_type={cov_type}.png"
+    )
+
 
 if __name__ == "__main__":
     main()
